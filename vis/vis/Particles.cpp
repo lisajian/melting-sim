419e722d2adc920b14d58041e47e86fc0b832c01
Lisa Jian <lisajian@berkeley.edu>
2017-04-30 14:03:34 -0700

    Attempting tensile instability

--- vis/vis/Particles.cpp
+++ vis/vis/Particles.cpp
@@ -25,7 +25,7 @@ void Particles::reset() {
     int num = 0;
     int nx = 2;
     int ny = 1;
-    int nz = 1;
+    int nz = 2;
     float y_offset = 1;
     float z_offset = -2;
     float d = 0.1;
@@ -45,32 +45,97 @@ void Particles::reset() {
 
 // Single update step for all particles
 void Particles::step() {
+    double h = 3; // TODO: Remove hardcoding
+
     // Generic time step update
     for (auto &p : particles) {
         p.vdt = p.vdt + p.forces / (double) p.mass;
         p.last_pos = p.curr_pos;
         p.curr_pos = p.curr_pos + p.vdt;
-        bbox.collides(p);
+        // bbox.collides(p);
     }
 
     // Get neighbors. Distance between point centers is hardcoded
     // double inf = std::numeric_limits<double>::infinity();
     for (auto &p : particles) {
-        find_neighboring(3, p);
+        find_neighboring(h, p);
+    }
+
+    // Increase solver iterations as necessary
+    double rho = 1; // TODO: Move this default rho elsewhere
+    double eps = 0.05; // TODO: Move this default elsewhere
+
+    // Constants used for s_corr
+    // TODO: Move these elsewhere
+    double k = 0.1;
+    double n = 4.0;
+    double del_q = 0.1 * h;
+
+    int solverIterations = 1;
+    for (int i = 0; i < solverIterations; i++) {
+        // Determine lambda_i
+        for (auto &p : particles) {
+            // Use poly6 kernel for rho (pressure)
+            double rho_i = 0.0;
+            for (auto &n : p.neighbors) {
+                glm::dvec3 diff = p.curr_pos - n.curr_pos;
+                rho_i += std::pow(h * h - glm::dot(diff, diff), 3.0);
+            }
+            rho_i *= 315.0 / (64.0 * M_PI * std::pow(h, 9.0));
+            p.C = rho_i / rho - 1.0;
+
+            // Use spiky kernel for gradients
+            glm::dvec3 grad_i_W = glm::dvec3(0, 0, 0);
+            double grad_j_W = 0.0;
+            for (auto &n : p.neighbors) {
+                glm::dvec3 diff = p.curr_pos - n.curr_pos;
+                double len = glm::length(diff);
+                glm::dvec3 intermed = (pow(h - len, 2.0) / len) * diff;
+                intermed *= - 45.0 / (M_PI * std::pow(h, 6.0));
+                
+                grad_i_W += intermed;
+                grad_j_W += glm::dot(intermed, intermed);
+            }
+            p.lambda = - p.C / (grad_j_W + glm::dot(grad_i_W, grad_i_W) + eps);
+        }
+        // Determine delta p
+        for (auto &p : particles) {
+            glm::dvec3 new_del_p = glm::dvec3(0, 0, 0);
+            for (auto &n : p.neighbors) {
+                glm::dvec3 diff = p.curr_pos - n.curr_pos;
+
+                // Get the gradient
+                double len = glm::length(diff);
+                glm::dvec3 intermed = (pow(h - len, 2.0) / len) * diff;
+                intermed *= - 45.0 / (M_PI * std::pow(h, 6.0));
+
+                // Calculate s_corr using poly6 kernel
+                double s_corr = std::pow(h * h - glm::dot(diff, diff), 3.0);
+                s_corr *= 315.0 / (64.0 * M_PI * std::pow(h, 9.0));
+
+                new_del_p += (p.lambda + n.lambda + s_corr) * intermed;
+            }
+            p.del_p = new_del_p / rho;
+        }
+    }
+
+    for (auto &p : particles) {
+        p.curr_pos += p.del_p;
+        bbox.collides(p);
     }
 }
 
 // Find all neighbors n of p such that the distance between the
 // centers of n and p is at most h
 void Particles::find_neighboring(double h, Particle &p) {
-  p.neighbors.clear();
-  // Naive way of finding neighbors
-  int j = 0;
-  for (auto &other : particles) {
-    if (&p != &other && glm::length(p.curr_pos - other.curr_pos) <= h) {
-        p.neighbors.push_back(other);
+    p.neighbors.clear();
+    // Naive way of finding neighbors
+    int j = 0;
+    for (auto &other : particles) {
+        if (&p != &other && glm::length(p.curr_pos - other.curr_pos) <= h) {
+            p.neighbors.push_back(other);
+        }
     }
-  }
 }
 
 void Particles::render() const
--- vis/vis/particle.h
+++ vis/vis/particle.h
@@ -24,12 +24,17 @@ struct Particle {
 
     glm::dvec3 last_pos;
     glm::dvec3 curr_pos;
+    glm::dvec3 del_p;
     glm::dvec3 vdt;
     glm::dvec3 new_vdt;
     glm::dvec3 forces;
     glm::dvec3 adjustment_vec;
     std::vector<Particle> neighbors;
-    float mass = 1000; // TODO: Masses shouldn't be this heavy...
+    float mass = 100000; // TODO: Masses shouldn't be this heavy...
+    double C = 0.0;
+    double lambda = 0.0;
+
+    // TODO: need a predicted position, pos*
 
 };
 
